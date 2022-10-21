3befd603e143ab1e6359cf0853a809d508d906d9
hnslmp <ancemo14@gmail.com>
2022-10-21 18:13:35 +0700

    Initial Commit

---
+++ MovieDbViper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDbViper
+//
+//  Created by Hansel Matthew on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
